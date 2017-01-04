1c0b5343d2baca8f6e57479722343130739463c6
Andrew Parrish <m.andrewparrish@gmail.com>
2017-01-03 23:46:31 -0500

    Backwards bool check

--- config/initializers/schedule.rb
+++ config/initializers/schedule.rb
@@ -1,6 +1,6 @@
 require Rails.root.join('lib', 'scheduler')
 
 scheduler = Scheduler.get_scheduler
-unless Rails.env == "production"
+if Rails.env == "production"
   Scheduler.add_event('1m', UpdateStations)
 end
